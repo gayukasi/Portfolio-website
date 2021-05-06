b8562f6b6e076179210d38cd8362a5be70fe78ba
Gayukasi <gayathrikasilingam@gmail.com>
2021-05-06 16:10:22 +0530

    changes in projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,6 +61,17 @@
   onscroll(document, navbarlinksActive)
 
   /**
+   * Play Video
+   */
+
+  $(document).ready(function() {
+    $('#headerVideoLink').magnificPopup({
+      type:'inline',
+      midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    });         
+  });
+
+  /**
    * Scrolls to an element with header offset
    */
   const scrollto = (el) => {
